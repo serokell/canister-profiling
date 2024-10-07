643266c1bef4759e62d16d4312ff274dcab48804
Andrei Borzenkov <root@sandwitch.dev>
2024-10-07 16:03:44 +0400

    [DMS-72] Implement benchmarks for PersistentOrderedMap
    This is just an experiment and isn't suitable for
    merging to the upstream in the current state.
    
    Modifications in collections/ folder compare
    PersistentOrderedMap with RBTree. Second test show
    performance of functions from PersistentOrderedMap,
    that have no analogue in RBTree module.

--- collections/motoko/tests/perf.sh
+++ collections/motoko/tests/perf.sh
@@ -5,11 +5,13 @@ let hashmap = install(wasm_profiling("../.dfx/local/canisters/hashmap/hashmap.wa
 let triemap = install(wasm_profiling("../.dfx/local/canisters/triemap/triemap.wasm"), null, null);
 let rbtree = install(wasm_profiling("../.dfx/local/canisters/rbtree/rbtree.wasm"), null, null);
 let splay = install(wasm_profiling("../.dfx/local/canisters/splay/splay.wasm"), null, null);
+let persistentmap = install(wasm_profiling("../.dfx/local/canisters/persistentmap/persistentmap.wasm"), null, null);
 
 call hashmap.__toggle_tracing();
 call triemap.__toggle_tracing();
 call rbtree.__toggle_tracing();
 call splay.__toggle_tracing();
+call persistentmap.__toggle_tracing();
 call hashmap.generate(50000);
 let _ = get_memory(hashmap);
 call triemap.generate(50000);
@@ -18,15 +20,19 @@ call rbtree.generate(50000);
 let _ = get_memory(rbtree);
 call splay.generate(50000);
 let _ = get_memory(splay);
+call persistentmap.generate(50000);
+let _ = get_memory(persistentmap);
 
 call hashmap.__toggle_tracing();
 call triemap.__toggle_tracing();
 call rbtree.__toggle_tracing();
 call splay.__toggle_tracing();
+call persistentmap.__toggle_tracing();
 call hashmap.batch_get(50);
 call triemap.batch_get(50);
 call rbtree.batch_get(50);
 call splay.batch_get(50);
+call persistentmap.batch_get(50);
 
 call hashmap.batch_put(50);
 let _ = get_memory(hashmap);
@@ -36,9 +42,11 @@ call rbtree.batch_put(50);
 let _ = get_memory(rbtree);
 call splay.batch_put(50);
 let _ = get_memory(splay);
+call persistentmap.batch_put(50);
+let _ = get_memory(persistentmap);
 
 call hashmap.batch_remove(50);
 call triemap.batch_remove(50);
 call rbtree.batch_remove(50);
 call splay.batch_remove(50);
-
+call persistentmap.batch_remove(50);
