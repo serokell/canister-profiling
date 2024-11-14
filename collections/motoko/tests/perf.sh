d50b9c77f7a672479171ee5333bf459a9da2df89
Pavel Golovin <pavel.golovin@serokell.io>
2024-11-14 19:18:01 +0100

    OrderedMap/Set: final benchmarks

--- collections/motoko/tests/perf.sh
+++ collections/motoko/tests/perf.sh
@@ -5,11 +5,15 @@ let hashmap = install(wasm_profiling("../.dfx/local/canisters/hashmap/hashmap.wa
 let triemap = install(wasm_profiling("../.dfx/local/canisters/triemap/triemap.wasm"), null, null);
 let rbtree = install(wasm_profiling("../.dfx/local/canisters/rbtree/rbtree.wasm"), null, null);
 let splay = install(wasm_profiling("../.dfx/local/canisters/splay/splay.wasm"), null, null);
+let orderedmap = install(wasm_profiling("../.dfx/local/canisters/orderedmap/orderedmap.wasm"), null, null);
+let orderedset = install(wasm_profiling("../.dfx/local/canisters/orderedmap/orderedset.wasm"), null, null);
 
 call hashmap.__toggle_tracing();
 call triemap.__toggle_tracing();
 call rbtree.__toggle_tracing();
 call splay.__toggle_tracing();
+call orderedmap.__toggle_tracing();
+call orderedset.__toggle_tracing();
 call hashmap.generate(50000);
 let _ = get_memory(hashmap);
 call triemap.generate(50000);
@@ -18,11 +22,17 @@ call rbtree.generate(50000);
 let _ = get_memory(rbtree);
 call splay.generate(50000);
 let _ = get_memory(splay);
+call orderedmap.generate(50000);
+let _ = get_memory(orderedmap);
+call orderedset.generate(50000);
+let _ = get_memory(orderedset);
 
 call hashmap.__toggle_tracing();
 call triemap.__toggle_tracing();
 call rbtree.__toggle_tracing();
 call splay.__toggle_tracing();
+call orderedmap.__toggle_tracing();
+call orderedset.__toggle_tracing();
 call hashmap.batch_get(50);
 call triemap.batch_get(50);
 call rbtree.batch_get(50);
@@ -36,9 +46,16 @@ call rbtree.batch_put(50);
 let _ = get_memory(rbtree);
 call splay.batch_put(50);
 let _ = get_memory(splay);
+call orderedmap.generate(50);
+let _ = get_memory(orderedmap);
+call orderedset.generate(50);
+let _ = get_memory(orderedset);
 
 call hashmap.batch_remove(50);
 call triemap.batch_remove(50);
 call rbtree.batch_remove(50);
 call splay.batch_remove(50);
+call orderedmap.batch_remove(50);
+call orderedset.batch_remove(50);
+
 
--- collections/perf.sh
+++ collections/perf.sh
@@ -13,6 +13,11 @@ let heap = wasm_profiling("motoko/.dfx/local/canisters/heap/heap.wasm", mo_confi
 let buffer = wasm_profiling("motoko/.dfx/local/canisters/buffer/buffer.wasm", mo_config);
 let vector = wasm_profiling("motoko/.dfx/local/canisters/vector/vector.wasm", mo_config);
 
+let orderedmap = wasm_profiling("motoko/.dfx/local/canisters/orderedmap/orderedmap.wasm", mo_config);
+let orderedset = wasm_profiling("motoko/.dfx/local/canisters/orderedset/orderedset.wasm", mo_config);
+let trieset = wasm_profiling("motoko/.dfx/local/canisters/trieset/trieset.wasm", mo_config);
+
+/*
 let rs_config = record { start_page = 1; page_limit = 128 };
 let hashmap_rs = wasm_profiling("rust/.dfx/local/canisters/hashmap/hashmap.wasm", rs_config);
 let btreemap_rs = wasm_profiling("rust/.dfx/local/canisters/btreemap/btreemap.wasm", rs_config);
@@ -22,13 +27,14 @@ let heap_stable_rs = wasm_profiling("rust/.dfx/local/canisters/heap_stable/heap_
 let imrc_hashmap_rs = wasm_profiling("rust/.dfx/local/canisters/imrc_hashmap/imrc_hashmap.wasm", rs_config);
 let vector_rs = wasm_profiling("rust/.dfx/local/canisters/vector/vector.wasm", rs_config);
 let vector_stable_rs = wasm_profiling("rust/.dfx/local/canisters/vector_stable/vector_stable.wasm", rs_config);
+*/
 
 //let movm_rs = wasm_profiling("rust/.dfx/local/canisters/movm/movm.wasm");
 //let movm_dynamic_rs = wasm_profiling("rust/.dfx/local/canisters/movm_dynamic/movm_dynamic.wasm");
 
 let file = "README.md";
 
-function perf(wasm, title, init, batch) {
+function perf_gen(wasm, title, init, batch) {
   let cid = install(wasm, encode (), null);
 
   output(file, stringify("|", title, "|", wasm.size(), "|"));
@@ -63,19 +69,114 @@ function perf(wasm, title, init, batch) {
   uninstall(cid);
 };
 
-let init_size = 1_000_000;
-let batch_size = 50;
-output(file, "\n## Map\n\n| |binary_size|generate 1m|max mem|batch_get 50|batch_put 50|batch_remove 50|upgrade|\n|--:|--:|--:|--:|--:|--:|--:|--:|\n");
-perf(hashmap, "hashmap", init_size, batch_size);
-perf(triemap, "triemap", init_size, batch_size);
-perf(rbtree, "rbtree", init_size, batch_size);
-perf(splay, "splay", init_size, batch_size);
-perf(btree, "btree", init_size, batch_size);
-perf(zhenya, "zhenya_hashmap", init_size, batch_size);
-perf(btreemap_rs, "btreemap_rs", init_size, batch_size);
-perf(imrc_hashmap_rs, "imrc_hashmap_rs", init_size, batch_size);
-perf(hashmap_rs, "hashmap_rs", init_size, batch_size);
+function perf_new_api(wasm, title, init) {
+  let cid = install(wasm, encode (), null);
 
+  output(file, stringify("|", init, "|"));
+  call cid.__toggle_tracing();
+  call cid.generate(init);
+
+  call cid.__toggle_tracing();
+  call cid.foldLeft();
+  let svg = stringify(title, "_foldLeft_", init, ".svg");
+  output(file, stringify("[", __cost__, "](", svg, ")|"));
+  flamegraph(cid, stringify(title, ".foldLeft"), svg);
+
+  call cid.foldRight();
+  let svg = stringify(title, "_foldRight_", init, ".svg");
+  output(file, stringify("[", __cost__, "](", svg, ")|"));
+  flamegraph(cid, stringify(title, ".foldRight"), svg);
+
+  call cid.mapfilter();
+  let svg = stringify(title, "_mapFilter_", init, ".svg");
+  output(file, stringify("[", __cost__, "](", svg, ")|"));
+  flamegraph(cid, stringify(title, ".mapfilter"), svg);
+
+  call cid.map();
+  let svg = stringify(title, "_map_", init, ".svg");
+  output(file, stringify("[", __cost__, "](", svg, ")|\n"));
+  flamegraph(cid, stringify(title, ".map"), svg);
+
+  uninstall(cid);
+};
+
+function perf_set_api(wasm, title, init, batch) {
+  let cid = install(wasm, encode (), null);
+
+  output(file, stringify("|", title, "|", init, "|"));
+  call cid.__toggle_tracing();
+  call cid.generate(init);
+
+  call cid.__toggle_tracing();
+  call cid.intersect(batch);
+  let svg = stringify(title, "_intersect.svg");
+  output(file, stringify("[", __cost__, "](",svg, ")|"));
+  flamegraph(cid, stringify(title, ".intersect"), svg);
+
+  call cid.union(batch);
+  let svg = stringify(title, "_union.svg");
+  output(file, stringify("[", __cost__, "](",svg, ")|"));
+  flamegraph(cid, stringify(title, ".union"), svg);
+
+  call cid.diff(batch);
+  let svg = stringify(title, "_diff.svg");
+  output(file, stringify("[", __cost__, "](",svg, ")|"));
+  flamegraph(cid, stringify(title, ".diff"), svg);
+
+  call cid.equals(batch);
+  let svg = stringify(title, "_equals.svg");
+  output(file, stringify("[", __cost__, "](",svg, ")|"));
+  flamegraph(cid, stringify(title, ".equals"), svg);
+
+  call cid.isSubset(batch);
+  let svg = stringify(title, "_isSubset.svg");
+  output(file, stringify("[", __cost__, "](",svg, ")|"));
+  flamegraph(cid, stringify(title, ".isSubset"), svg);
+
+  output(file, "\n");
+  uninstall(cid);
+};
+
+// let init_size = 1_000_000;
+let batch_size = 50;
+let sizes = vec {100; 1000; 10_000; 100_000; 1_000_000};
+
+function compare_gen(init_size) {
+  perf_gen(hashmap, stringify("hashmap+", init_size), init_size, batch_size);
+  perf_gen(triemap, stringify("triemap+", init_size), init_size, batch_size);
+  perf_gen(rbtree, stringify("rbtree+", init_size), init_size, batch_size);
+  perf_gen(splay, stringify("splay+", init_size), init_size, batch_size);
+  perf_gen(btree, stringify("btree+", init_size), init_size, batch_size);
+  perf_gen(zhenya, stringify("zhenya_hashmap+", init_size), init_size, batch_size);
+  perf_gen(orderedmap, stringify("orderedmap+", init_size), init_size, batch_size);
+  perf_gen(orderedset, stringify("orderedset+", init_size), init_size, batch_size);
+
+  // perf(btreemap_rs, stringify("btreemap_rs"), init_size, batch_size);
+  // perf(imrc_hashmap_rs, stringify("imrc_hashmap_rs"), init_size, batch_size);
+  // perf(hashmap_rs, stringify("hashmap_rs"), init_size, batch_size);
+};
+
+function compare_new_api(init_size) {
+  perf_new_api(orderedmap, stringify("orderedmap+", init_size), init_size);
+  perf_new_api(orderedset, stringify("orderedset+", init_size), init_size);
+};
+
+function compare_set_api(init_size) {
+  perf_set_api(trieset, stringify("trieset+", init_size), init_size, batch_size);
+  perf_set_api(orderedset, stringify("orderedset+", init_size), init_size, batch_size);
+};
+
+
+output(file, stringify("\n## General API\n\n| |binary_size|generate|max mem|batch_get 50|batch_put 50|batch_remove 50|upgrade|\n|--:|--:|--:|--:|--:|--:|--:|--:|\n"));
+sizes.map(compare_gen);
+
+output(file, stringify("\n## New API\n\n|size|foldLeft|foldRight|mapfilter|map|\n|--:|--:|--:|--:|--:|\n"));
+sizes.map(compare_new_api);
+
+output(file, stringify("\n## Set API\n\n| |size|intersect|union|diff|equals|isSubset|\n|--:|--:|--:|--:|--:|--:|--:|\n"));
+sizes.map(compare_set_api);
+
+/*
 output(file, "\n## Priority queue\n\n| |binary_size|heapify 1m|max mem|pop_min 50|put 50|pop_min 50|upgrade|\n|--:|--:|--:|--:|--:|--:|--:|--:|\n");
 perf(heap, "heap", init_size, batch_size);
 perf(heap_rs, "heap_rs", init_size, batch_size);
@@ -97,7 +198,6 @@ perf(heap_stable_rs, "heap_stable_rs", init_size, batch_size);
 perf(vector_rs, "vec_rs", init_size, batch_size);
 perf(vector_stable_rs, "vec_stable_rs", init_size, batch_size);
 
-/*
 let movm_size = 10000;
 output(file, "\n## MoVM\n\n| |binary_size|generate 10k|max mem|batch_get 50|batch_put 50|batch_remove 50|\n|--:|--:|--:|--:|--:|--:|--:|\n");
 perf(hashmap, "hashmap", movm_size);
